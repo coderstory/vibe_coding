97ef47aa05c88012bd67d962a5714e80a8a0ab81
coderstory <coderstory@163.com>
2026-04-03 02:10:22 +0800

    feat(phase-3): WebConfig 和 MyBatis Mapper

---
+++ springboot/src/main/java/cn/coderstory/springboot/config/WebConfig.java
@@ -0,0 +1,25 @@
+package cn.coderstory.springboot.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.APPLICATION_JSON);
+    }
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        StandardServletMultipartResolver resolver = new StandardServletMultipartResolver();
+        return resolver;
+    }
+}
