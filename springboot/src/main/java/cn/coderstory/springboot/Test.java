0b9273a19cceefd00a804d75aa1a304e231c120b
coderstory <coderstory@163.com>
2026-04-20 12:17:08 +0800

    test: 添加密码加密测试类

--- springboot/src/main/java/cn/coderstory/springboot/Test.java
+++ springboot/src/main/java/cn/coderstory/springboot/Test.java
@@ -1,1 +1,7 @@
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; public class Test { public static void main(String[] args) { System.out.println(new BCryptPasswordEncoder().encode("admin123")); } }
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(new BCryptPasswordEncoder().encode("admin123"));
+    }
+}
